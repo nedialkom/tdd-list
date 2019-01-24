2f6c98b6d739a645cee51eb4140dbbbb4fe3c091
nedialkom <nedialkom@gmail.com>
2019-01-24 10:33:18 +0200

    Fix3?

--- superlists/settings.py
+++ superlists/settings.py
@@ -30,7 +30,7 @@ if 'DJANGO_DEBUG_FALSE' in os.environ:
     DEBUG = False
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
     #ALLOWED_HOSTS = [os.environ['SITENAME']]
-    ALLOWED_HOSTS = ['nedialko.tk']
+    ALLOWED_HOSTS = ['*']
 else:
     DEBUG = True
     SECRET_KEY = 'insecure-key-for-dev'
